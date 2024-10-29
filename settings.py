92c9888db71fbe81e25a930e012f59237e5fbda4
lim <mr.shamil.ibragimov.2000@mail.ru>
2024-10-29 16:55:39 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,20 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # ERP Modules
+    'inventory',
+    'sales',
+    'purchases',
+    'crm',
+    'finance',
+    'hr',
+    'projects',
+    'analytics',
+    'logistics',
+    'user_management',
+    'settings',
+]
